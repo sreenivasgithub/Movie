e50a4c7909df101639b280dd138925c32389abb9
sreenivas <sreenivasareddy.kr@gmail.com>
2019-12-24 19:35:03 +0530

    Comitted All Files

--- MovieProject/MovieReviewApp/urls.py
+++ MovieProject/MovieReviewApp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from .import views
-
+# from pdb import set_trace;set_trace()
 urlpatterns = [
     url(r'admin', admin.site.urls),
     url(r'index', views.index, name='index'),
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'result', views.result, name='result'),
     url(r'get_data', views.get_data, name='get_data'),
     url(r'single_movie', views.single_movie, name='single_movie'),
+    url(r'update', views.update_movie, name='update'),
 ]
--- MovieProject/MovieReviewApp/views.py
+++ MovieProject/MovieReviewApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import MovieInfo
 # Create your views here.
 def index(request):
@@ -15,13 +15,36 @@ def result(request):
 
         movie = MovieInfo(movie_name=movie_name, movie_type=movie_type, movie_review=movie_review, movie_date=movie_date, movie_details=movie_details)
         movie.save()
-        return render(request, 'index.html')
+        return render(request, 'get_data.html')
     else:
         return HttpResponse('Error')
 def get_data(request):
     fetch = MovieInfo.objects.all()
     return render(request, 'get_data.html', {'fetch':fetch})
+
 def single_movie(request):
-    id = request.GET.get('id')
+    # from pdb import set_trace;
+    # set_trace()
+    id = (request.GET.get('id'))
     movie = MovieInfo.objects.get(id=id)
-    return render(request, 'single_movie.html', {'movie': movie})
+    #movie = get_object_or_404(MovieInfo, id=id)
+    return render(request, 'update.html', {'movie': movie})
+# from pdb import set_trace;set_trace()
+
+def update_movie(request):
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@", request)
+    from pdb import set_trace;
+    set_trace()
+    if request.method == 'POST':
+        id = request.GET.get('id')
+        #item = get_object_or_404(MovieInfo, id=id)
+        item = MovieInfo.objects.filter(pk=3)
+        item.movie_name = request.POST['name']
+        item.movie_type = request.POST['type']
+        item.movie_review = request.POST['review']
+        item.movie_date = request.POST['date']
+        item.movie_details = request.POST['details']
+        item.save()
+        return HttpResponse('Updated')
+    else:
+        return HttpResponse('ErrorUpdate')
