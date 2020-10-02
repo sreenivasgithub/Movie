77fa55a51840099fa808d13c99e9ddf19c65f157
Sreenivas <sreenivasareddy.kr@gmail.com>
2020-10-02 13:15:10 +0530

    about static files

--- MovieProject/MovieReviewApp/urls_api.py
+++ MovieProject/MovieReviewApp/urls_api.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 from .import views_api
 # from pdb import set_trace;set_trace()
 
 urlpatterns = [
-    url(r'^api/', views_api.movieList.as_view())
+    url(r'^movieList/', views_api.movieList.as_view()),
+    path('get_single/<str:pk>/', views_api.get_single, name='get_single'),
+    path('deleteMovie/<str:pk>/', views_api.deleteMovie, name='deleteMovie'),
 ]
--- MovieProject/MovieReviewApp/views_api.py
+++ MovieProject/MovieReviewApp/views_api.py
@@ -2,12 +2,24 @@ from .serializers import movieinfoserializers
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-#from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view
 from .models import MovieInfo
 
-#@api_view(['GET',])
+
 class movieList(APIView):
     def get(self, request):
         movie_info = MovieInfo.objects.all()
         serializer = movieinfoserializers(movie_info, many=True)
         return Response(serializer.data)
+
+@api_view(['GET'])
+def get_single(request, pk):
+    single_movie_info = MovieInfo.objects.get(id=pk)
+    serializer = movieinfoserializers(single_movie_info, many=False)
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteMovie(request, pk):
+    single_movie = MovieInfo.objects.get(id=pk)
+    single_movie.delete()
+    return Response('Deleted Successfully')
